09a7f24292d9efdc5793b34eb74559fbd9296366
Otávio Fernandes <ofernandes@schubergphilis.com>
2018-10-13 23:00:56 +0200

    Apply command help message.

--- cmd/hosts/apply.go
+++ cmd/hosts/apply.go
@@ -9,9 +9,19 @@ import (
 
 var applyCmd = &cobra.Command{
 	Use:   "apply",
-	Short: "Generate a `/etc/hots` formated file in configured location.",
-	Long:  `Gather all '.host' files and compose a unified version of it.`,
-	Run:   runApplyCmd,
+	Short: "Generate and write the final '/etc/hosts' file.",
+	Long: `
+By inspecting 'hosts.baseDirectory', 'apply' command will compose a unified version to be stored
+at '/etc/hosts' file, or another configurable location. Every file name found in 'baseDirectory' is
+kept as a comment before it's contents.
+
+On reading the input files, it's applied sanitization to only bring strings that are starting with an
+IPv4 or IPv6 address, on IPv6 format we also include OS X interface name aliasing, using '%'
+notation.
+
+Please not you may also need the companion of 'sudo' to execute 'hosts apply' action.
+`,
+	Run: runApplyCmd,
 }
 
 func init() {
