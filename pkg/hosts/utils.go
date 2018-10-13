0d24e80bd1b3b02cb920ecf079bc7e4265c94211
Otávio Fernandes <ofernandes@schubergphilis.com>
2018-10-13 10:54:51 +0200

    Utls.

---
+++ pkg/hosts/utils.go
@@ -0,0 +1,39 @@
+package hosts
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+// readFile Wrap up a ioutil call, using fatal log in case of error.
+func readFile(path string) []byte {
+	log.Printf("Reading file: '%s'", path)
+
+	if !exists(path) {
+		log.Fatalf("Can't find file: '%s'", path)
+	}
+
+	fileBytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return fileBytes
+}
+
+// isDir Check if informed path is a directory, boolean return.
+func isDir(dirPath string) bool {
+	stat, err := os.Stat(dirPath)
+	if err != nil {
+		return false
+	}
+	return stat.IsDir()
+}
+
+// exists Check if path exists, boolean return.
+func exists(path string) bool {
+	if _, err := os.Stat(path); err != nil {
+		return false
+	}
+	return true
+}
